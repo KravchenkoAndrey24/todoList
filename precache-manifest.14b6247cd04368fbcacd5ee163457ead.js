f18e681f334eee90161ef267bfff976fe3b1c538
yazaandrey@gmail.com <yazaandrey@gmail.com>
2021-03-11 17:43:51 +0200

    Updates

---
+++ precache-manifest.14b6247cd04368fbcacd5ee163457ead.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b9b3435db2e54a7db7dc34b9eb5c5ea7",
+    "url": "/todoList/index.html"
+  },
+  {
+    "revision": "fb772a8c8144c1d9bfbf",
+    "url": "/todoList/static/css/main.f6822878.chunk.css"
+  },
+  {
+    "revision": "d8b49394a4ef110c78b8",
+    "url": "/todoList/static/js/2.0f2e4cd6.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/todoList/static/js/2.0f2e4cd6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fb772a8c8144c1d9bfbf",
+    "url": "/todoList/static/js/main.b66a4486.chunk.js"
+  },
+  {
+    "revision": "c39c464575b971d22f9a",
+    "url": "/todoList/static/js/runtime-main.67bcfed6.js"
+  }
+]);
